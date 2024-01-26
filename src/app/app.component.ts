b8aa1a47ca787a20a2eb7b1539dcd38b992951b2
dayannabravo <hildabravo2001@gmail.com>
2024-01-25 19:14:37 -0500

    changes
    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,11 @@ import { RouterOutlet } from '@angular/router';
 import { Parte1Component } from './components/parte1/parte1.component';
 import { Parte2Component } from './components/parte2/parte2.component';
 import { Parte3Component } from './components/parte3/parte3.component';
-import { Parte4Component } from './components/parte4/parte4.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Parte1Component, Parte2Component, Parte3Component, Parte4Component],
+  imports: [CommonModule, RouterOutlet, Parte1Component, Parte2Component, Parte3Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
